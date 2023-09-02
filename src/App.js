ca63a1c958f5fbb54caea58f97a6caee4d28ff5d
NagendraVarma1 <nagendravarmal512>
2023-09-02 13:21:49 +0530

    Small changes

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
   const readingInputRef = useRef();
   const speakingInputRef = useRef();
   const writingInputRef = useRef();
-  const tutionFeeInputRef = useRef();
-  const gicFeeInputRef = useRef();
+  const tutionFeeInputRef = useRef(0);
+  const gicFeeInputRef = useRef(0);
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
@@ -41,41 +41,41 @@ function App() {
     const reading = readingInputRef.current.value;
     const speaking = speakingInputRef.current.value;
     const writing = writingInputRef.current.value;
-    const tution = payForTution;
     const tutionFee = tutionFeeInputRef.current.value;
-    const gic = doGic;
     const gicFee = gicFeeInputRef.current.value;
 
     const config = {
       SecureToken: "a9f4f8e0-6c5a-4dbf-aaf5-38b3c19c16eb",
       To: `${email}`,
       From: "nagendravarmal512@gmail.com",
-      Subject: "Response of the Form",
-      Body: `Email: ${email},
-      Name: ${name},
-      Age: ${age}, 
-      Education: ${education}, 
-      Institute: ${institute}, 
-      Study: ${study}, 
-      Experience: ${experience}, 
-      Institute Name in Canada: ${canadaInstitute}, 
-      Program Name: ${program}, 
-      Your Country: ${country}, 
-      Future goals: ${goals}, 
-      English Listening - score: ${listening}, 
-      English Reading - score: ${reading}, 
-      English Speaking - score: ${speaking}, 
-      English Writing - score: ${writing}, 
-      Did you pay your first year tution: ${tution}, 
-      Paid tution fee: ${tutionFee}, 
-      Did you do a GIC: ${gic}, 
-      Paid GIC fee: ${gicFee},`,
+      Subject: `Statement of Purpose for ${name}`,
+      Body: `I would like to take this opportunity to introduce myself as ${name} and age ${age} from ${country}, a passionate individual with a strong desire to persue a career in the field of Testing. I have recently completed my ${education} in ${institute}, where I gained a solid foundation in the principles and practices of software testing. After careful considerationof my alternatives for the further studies, I have chosen to pursue the ${program} program at ${canadaInstitute}. This program stood out to me for several reasons. Firstly, the curriculum is highly interdisciplinary, which will provide me with a comprehensive and practical understanding of all major concepts in software testing. In terms of my academic background, my ${education} in ${institute} has equipped me with a strong foundation in ${study}. This background align perfectly with the ${program} program. Additionally, I have achieved an overall IELTS score of ${listening} in listening, ${reading} in reading, ${writing} in writing, and ${speaking} in speaking, demonstrating my proficiency in the English language.And my experince is ${experience}. Upon Completion of the ${program}, my goal is ${goals}. In terms of finances, I have paid the first year's tution fee of ${tutionFee} CAD and have a Guaranteed Investment Certificate (GIC) of ${gicFee} CAD to cover my living expenses. Additionally, my family is providing financial support for my education in Canada.  I kindely request you to process my Visa application as soon as possible. I am greatful for your time and cosideration. Thank you `,
+
+      // Email: ${email},
+      // Name: ${name},
+      // Age: ${age},
+      // Education: ${education},
+      // Institute: ${institute},
+      // Study: ${study},
+      // Experience: ${experience},
+      // Institute Name in Canada: ${canadaInstitute},
+      // Program Name: ${program},
+      // Your Country: ${country},
+      // Future goals: ${goals},
+      // English Listening - score: ${listening},
+      // English Reading - score: ${reading},
+      // English Speaking - score: ${speaking},
+      // English Writing - score: ${writing},
+      // Did you pay your first year tution: ${tution},
+      // Paid tution fee: ${tutionFee},
+      // Did you do a GIC: ${gic},
+      // Paid GIC fee: ${gicFee},
     };
     console.log(config);
     if (window.Email) {
       window.Email.send(config).then(() =>
         alert(
-          "Form Submitted Sucsessfully and please check your mail for the response"
+          "Form Submitted Sucsessfully and please check your mail or spam box for the response"
         )
       );
     }
@@ -112,9 +112,20 @@ function App() {
         width: "100%",
         height: "100%",
         paddingTop: "20px",
+        paddingBotton: "20px",
       }}
     >
       <div className={classes.header}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="white"
+          class="bi bi-airplane"
+          viewBox="0 0 16 16"
+        >
+          <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Zm.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1c-.213 0-.458.158-.678.599Z" />
+        </svg>
         <h1>EFFIZIENT</h1>
       </div>
       <div className={classes.container}>
@@ -378,13 +389,24 @@ function App() {
             How much tution fee did you pay?
             <span style={{ color: "red" }}>*</span>
           </Form.Label>
-          <Form.Control
-            className="w-50 m-2"
-            type="text"
-            placeholder="your answer"
-            ref={tutionFeeInputRef}
-            required
-          />
+          {payForTution === "Yes" ? (
+            <Form.Control
+              className="w-50 m-2"
+              type="text"
+              placeholder="your answer"
+              ref={tutionFeeInputRef}
+              required
+            />
+          ) : (
+            <Form.Control
+              disabled
+              className="w-50 m-2"
+              type="text"
+              placeholder="your answer"
+              ref={tutionFeeInputRef}
+              value={"0"}
+            />
+          )}
         </Form.Group>
         <Form.Group className={classes.form}>
           <Form.Label>
@@ -411,13 +433,24 @@ function App() {
             How much did you pay towards GIC?
             <span style={{ color: "red" }}>*</span>
           </Form.Label>
-          <Form.Control
-            className="w-50 m-2"
-            type="text"
-            placeholder="your answer"
-            required
-            ref={gicFeeInputRef}
-          />
+          {doGic === "Yes" ? (
+            <Form.Control
+              className="w-50 m-2"
+              type="text"
+              placeholder="your answer"
+              required
+              ref={gicFeeInputRef}
+            />
+          ) : (
+            <Form.Control
+              disabled
+              className="w-50 m-2"
+              type="text"
+              placeholder="your answer"
+              value={0}
+              ref={gicFeeInputRef}
+            />
+          )}
         </Form.Group>
         <div className={classes.btn}>
           <Button type="submit">Submit</Button>
